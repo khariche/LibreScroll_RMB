6faa3e4dd1022e8df4d1197b6c4fa801f217dba2
user726687 <98432183+user726687@users.noreply.github.com>
2026-03-08 21:42:33 +0000

    update tag to v2.1.5 for release

--- main.zig
+++ main.zig
@@ -16,7 +16,7 @@ var raw_thread_pending_restart = false;
 const Vec2f = @Vector(2, f32);
 const Vec2i = @Vector(2, i32);
 
-const LIBRE_SCROLL_VERSION_TEXT = "v2.1.4";
+const LIBRE_SCROLL_VERSION_TEXT = "v2.1.5";
 const MAGIC_WORD: [8]u8 = ("PASS" ++ .{0} ** 4).*;
 const WM_TRAY = 0x8001;
 const WM_RAW_STOPPED = 0x8002;
