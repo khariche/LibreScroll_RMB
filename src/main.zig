9932716147cc88cc9b5d9558c969b917e54953c7
khariche <47947345+khariche@users.noreply.github.com>
2026-06-19 14:53:25 +0200

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -32,9 +32,187 @@ pub fn main() void {
 
     if (.NULL == SetThreadDpiAwarenessContext(.UNAWARE_GDISCALED)) return;
 
-    const hwndTray = CreateDialogParamA(null, "CFGDLG", null, trayProc, 0) orelse return;
+    const hwndTray = CreateDialogParamA(null, "CFGDLG", null, @ptrCast(&trayProc), 0) orelse return;
 
     const hSensY = GetDlgItem(hwndTray, 0x4002) orelse return;
     const hSensX = GetDlgItem(hwndTray, 0x4003) orelse return;
     _ = SetWindowLongPtrA(hSensY, -21, SetWindowLongPtrA(hSensY, -4, @bitCast(@intFromPtr(&inputProc))));
-    _ = SetWindowLongPtrA(hSensX, -21, SetWindowLong
+    _ = SetWindowLongPtrA(hSensX, -21, SetWindowLongPtrA(hSensX, -4, @bitCast(@intFromPtr(&inputProc))));
+
+    const ico = ico: {
+        const cpl = LoadLibraryA("main.cpl") orelse break :ico null;
+        defer _ = FreeLibrary(cpl);
+        break :ico LoadIconA(cpl, @ptrFromInt(608));
+    };
+
+    _ = SendMessageA(hwndTray, 0x0080, 0, @bitCast(@intFromPtr(ico))); 
+    _ = SendMessageA(hwndTray, 0x0080, 1, @bitCast(@intFromPtr(ico))); 
+
+    var tray_data: NOTIFYICONDATAA = .{
+        .hWnd = hwndTray,
+        .uID = TRAY_UID,
+        .uFlags = 0x8F,
+        .uCallbackMessage = WM_TRAY,
+        .hIcon = ico,
+        .uTimeout = 4,
+        .szTip = @splat(0),
+        .dwState = 0,
+        .dwStateMask = 1,
+    };
+    tray_data.szTip[0..11].* = "LibreScroll".*;
+
+    if (0 == Shell_NotifyIconA(.ADD, &tray_data)) return;
+    defer _ = Shell_NotifyIconA(.DELETE, &tray_data);
+    if (0 == Shell_NotifyIconA(.SETVERSION, &tray_data)) return;
+
+    if (!startThread()) return;
+
+    var msg: MSG = undefined;
+    while (GetMessageA(&msg, null, 0, 0) > 0) {
+        if (null == msg.hWnd) {
+            if (WM_RAW_STOPPED == msg.message) {
+                tray_data.szTip[11..23].* = " - Inactive\x00".*;
+                _ = Shell_NotifyIconA(.MODIFY, &tray_data);
+                if (GetDlgItem(hwndTray, 104)) |hPause| {
+                    _ = SetWindowTextA(hPause, "Unpause");
+                    _ = SetWindowLongA(hPause, -12, 105);
+                }
+                _ = CloseHandle(raw_thread_handle.?);
+                raw_thread_handle = null;
+                if (raw_thread_pending_restart) {
+                    raw_thread_pending_restart = false;
+                    _ = startThread(); 
+                }
+            } else if (WM_RAW_STARTED == msg.message) {
+                tray_data.szTip[11..21].* = " - Active\x00".*;
+                _ = Shell_NotifyIconA(.MODIFY, &tray_data);
+                if (GetDlgItem(hwndTray, 105)) |hUnpause| {
+                    _ = SetWindowTextA(hUnpause, "Pause");
+                    _ = SetWindowLongA(hUnpause, -12, 104);
+                }
+            }
+        } else if (0 == IsDialogMessageA(hwndTray, &msg)) {
+            _ = TranslateMessage(&msg);
+            _ = DispatchMessageA(&msg);
+        }
+    }
+}
+
+fn trayProc(hwnd: HWND, uMsg: u32, wParam: usize, lParam: isize) callconv(.C) isize {
+    switch (uMsg) {
+        else => return 0,
+        0x0010 => _ = ShowWindowAsync(hwnd, 0), 
+        0x0111 => onWmCommand(hwnd, wParam, lParam),
+        WM_TRAY => onWmTray(hwnd, wParam, lParam),
+    }
+    return 1;
+}
+
+fn inputProc(hwnd: HWND, uMsg: u32, wParam: usize, lParam: isize) callconv(.C) isize {
+    if (uMsg == 0x0102 and wParam >= ' ') {
+        switch(wParam) {
+            else => if (wParam != '-' or 0 != SendMessageA(hwnd, 0x00B0, 0, 0)) return 0,
+            '0'...'9' => {},
+        }
+    }
+    const proc: WNDPROC = @ptrFromInt(@as(usize, @bitCast(GetWindowLongPtrA(hwnd, -21))));
+    return CallWindowProcA(proc, hwnd, uMsg, wParam, lParam);
+}
+
+fn onWmCommand(hwnd: HWND, wParam: usize, lParam: isize) void {
+    const hCtrl: ?HWND = @ptrFromInt(@as(usize, @bitCast(lParam)));
+    const id = wParam & 0xFFFF;
+    const uMsg = wParam >> 16;
+    const is_accel = (null == hCtrl) and (1 == uMsg); _ = is_accel;
+    switch (id) {
+        else => {},
+        100 => quit(),
+        101 => show(hwnd),
+        102 => info(hwnd),
+        103 => elevate(),
+        104 => if (raw_thread_handle) |_| {
+            raw_thread_pending_restart = false;
+            _ = PostThreadMessageA(raw_thread_id, 0x0012, 0, 0);
+        },
+        105, 106 => {
+            if (106 == id) save(hwnd);
+            if (raw_thread_handle) |_| {
+                raw_thread_pending_restart = true;
+                _ = PostThreadMessageA(raw_thread_id, 0x0012, 0, 0);
+            } else {
+                raw_thread_pending_restart = false;
+                if (!startThread()) quit();
+            }
+        },
+    }
+}
+
+fn onWmTray(hwnd: HWND, wParam: usize, lParam: isize) void {
+    const src: packed struct(usize) {
+        msg: u16,
+        uid: u16,
+        _: if (8 == @sizeOf(usize)) u32 else u0,
+    } = @bitCast(lParam);
+    const pos: packed struct(usize) {
+        x: i16,
+        y: i16,
+        _: if (8 == @sizeOf(usize)) u32 else u0,
+    } = @bitCast(wParam);
+    switch (src.msg) {
+        else => {},
+        0x007B => menu(hwnd, src.uid, pos.x, pos.y), 
+        0x0400 => show(hwnd), 
+    }
+}
+
+fn elevate() void {
+    var buf: [32767:0]u8 = undefined;
+    const len = GetModuleFileNameA(null, &buf, buf.len); 
+    if (len == 0) return;
+    if (len == buf.len and 0 != GetLastError()) return;
+    _ = CloseHandle(process_mutex);
+    _ = ShellExecuteA(null, "runas", &buf, null, null, 0);
+    quit();
+}
+
+fn quit() void {
+    _ = PostQuitMessage(0);
+}
+
+fn info(hwnd: HWND) void {
+    _ = MessageBoxA(hwnd, "Visit https://github.com/EsportToys/LibreScroll for more info.", "About LibreScroll " ++ LIBRE_SCROLL_VERSION_TEXT, 0);
+}
+
+fn menu(hwnd: HWND, uid: u16, x: i16, y: i16) void {
+    const tray_hmenu = LoadMenuA(null, "menu") orelse return;
+    defer _ = DestroyMenu(tray_hmenu);
+    const hMenu = GetSubMenu(tray_hmenu, IsUserAnAdmin() | @as(i32, if (raw_thread_handle) |_| 2 else 0)) orelse return;
+    var rect: [4]i32 = undefined;
+    _ = Shell_NotifyIconGetRect(&.{ .hWnd = hwnd, .uID = uid }, &rect);
+    _ = SetForegroundWindow(hwnd);
+    _ = SetThreadDpiAwarenessContext(.PER_MONITOR_AWARE_V2);
+    _ = TrackPopupMenu(hMenu, 0, x, y, 0, hwnd, null);
+    _ = SetThreadDpiAwarenessContext(.NULL);
+}
+
+fn show(hwnd: HWND) void {
+    _ = SetDlgItemInt(  hwnd, 0x4001, @bitCast(global_config.decay), 0 );
+    _ = SetDlgItemInt(  hwnd, 0x4002, @bitCast(global_config.sensY), 1 );
+    _ = SetDlgItemInt(  hwnd, 0x4003, @bitCast(global_config.sensX), 1 );
+    _ = SetDlgItemInt(  hwnd, 0x4004, @bitCast(global_config.stepY), 0 );
+    _ = SetDlgItemInt(  hwnd, 0x4005, @bitCast(global_config.stepX), 0 );
+    _ = CheckDlgButton( hwnd, 0x4006, @bitCast(global_config.flick)    );
+    _ = CheckDlgButton( hwnd, 0x4007, @bitCast(global_config.think)    );
+    if (0 == IsWindowVisible(hwnd)) _ = ShowWindowAsync(hwnd, 5);
+    _ = SetForegroundWindow(hwnd);
+}
+
+fn save(hwnd: HWND) void {
+    const ini = "./options.ini";
+    const sec = "LibreScroll";
+    var buf: [32767:0]u8 = undefined;
+    inline for (.{ "decay", "sensY", "sensX", "stepY", "stepX" }, 0x4001..) |key, i| {
+        _ = GetDlgItemTextA(hwnd, i, &buf, buf.len);
+        _ = WritePrivateProfileStringA(sec, key, &buf, ini);
+    }
+    _ = WritePrivateProfileStringA(sec, "flick", if (0 == IsDlgButtonChecked(hwnd,
